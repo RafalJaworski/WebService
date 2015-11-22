88f981252cdc65149bccff47ff4760bc17280684
RafalJaworski <admin@lux-price.pl>
2015-11-22 12:18:13 +0100

    new function

--- app/src/main/java/domenafirmy/webservice/MainActivity.java
+++ app/src/main/java/domenafirmy/webservice/MainActivity.java
@@ -3,19 +3,43 @@ package domenafirmy.webservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
+import retrofit.Callback;
 import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     //kliknij "adres bazowy webservice na clab.type
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //budujemy obj dostarczajacy nam implementacje naszych interfacow
         RestAdapter adapter   = new RestAdapter.Builder()
                 .setEndpoint("https://szkolenie-android-kontakty.appspot.com/_ah/api")
                 .setLogLevel(RestAdapter.LogLevel.FULL)
                 .build();
+
+        //z adaptera pobieramy implementacje concatcWebservice
+        ContactWebService webService = adapter.create(ContactWebService.class);
+
+        webService.listcontact(new Callback<List<Contact>>() {
+            @Override
+            public void success(List<Contact> contacts, Response response) {
+
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
     }
+
+
 }
