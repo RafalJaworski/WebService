17cace50b6a4d930896b6898a560e880c2471b97
RafalJaworski <admin@lux-price.pl>
2015-11-22 15:22:55 +0100

    onOptionsItemSelected implementation

--- app/src/main/java/domenafirmy/webservice/MainActivity.java
+++ app/src/main/java/domenafirmy/webservice/MainActivity.java
@@ -1,5 +1,6 @@
 package domenafirmy.webservice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -66,6 +67,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.open_form)
+        {
+            Intent intent  = new Intent(this, showActivity.class);
+            startActivity(intent);
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
