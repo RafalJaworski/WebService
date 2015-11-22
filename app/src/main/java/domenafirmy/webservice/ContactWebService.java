7366b13f1f0eb94259d5cf228d8e44f00c9d55ea
RafalJaworski <admin@lux-price.pl>
2015-11-22 12:10:07 +0100

    klass to get items form webservice

--- app/src/main/java/domenafirmy/webservice/ContactWebService.java
+++ app/src/main/java/domenafirmy/webservice/ContactWebService.java
@@ -9,4 +9,22 @@ import retrofit.http.GET;
 public interface ContactWebService {
     @GET("/list")
     public void listcontact (Callback<List<Contact>> callback);
+
+    //webservice nie zwraca nam listy wiec piszemy klase do zwracania
+    public static class ContactListResponse
+    {
+        private List<Contact> items;
+
+        public List<Contact> getItems() {
+            return items;
+        }
+
+        public void setItems(List<Contact> items) {
+            this.items = items;
+        }
+
+
+
+
+    }
 }
