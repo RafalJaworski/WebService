333075eb5dcc4cca4027de52c31c273bf18f77f7
RafalJaworski <admin@lux-price.pl>
2015-11-22 12:14:50 +0100

    Adapter for interfaces

--- app/src/main/java/domenafirmy/webservice/MainActivity.java
+++ app/src/main/java/domenafirmy/webservice/MainActivity.java
@@ -3,6 +3,8 @@ package domenafirmy.webservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import retrofit.RestAdapter;
+
 public class MainActivity extends AppCompatActivity {
 
     //kliknij "adres bazowy webservice na clab.type
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RestAdapter adapter   = new RestAdapter.Builder()
+                .setEndpoint("https://szkolenie-android-kontakty.appspot.com/_ah/api")
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .build();
     }
 }
